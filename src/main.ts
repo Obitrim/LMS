38a7705d607ff163839318b0480ad225dfd8e658
Obitrim <pevangelist516@gmail.com>
2024-07-19 16:57:36 +0000

    feat: install global components plugin

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/index.router'
+import GLOBAL_COMPONENTS_PLUGIN from './components'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(GLOBAL_COMPONENTS_PLUGIN)
 
 app.mount('#app')
