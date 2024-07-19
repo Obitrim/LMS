465ad650310009477f6f3b428bf31b0b3404e651
Obitrim <pevangelist516@gmail.com>
2024-07-19 16:58:15 +0000

    feat: create authentication pages

---
+++ src/components/auth/AuthLogin.vue
@@ -0,0 +1,45 @@
+<template>
+  <title>Login | LMS</title>
+
+  <div class="w-full">
+    <h1 :class="['text-3xl mb-5']">Login</h1>
+
+    <form class="">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email"> Email </label>
+        <input
+          class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+          id="email"
+          type="email"
+          placeholder="email"
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password"> Password </label>
+        <input
+          class="appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          class="bg-charcoal hover:bg-charcoal/90 transition-all duration-150 text-white font-bold py-2 px-4 rounded focus:outline-none"
+          type="button"
+        >
+          Login
+        </button>
+        <a
+          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
