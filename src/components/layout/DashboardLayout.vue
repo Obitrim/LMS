55ad4a743df3283991acab9162a9e05fb9e5ae3d
Obitrim <pevangelist516@gmail.com>
2024-07-29 16:51:49 +0000

    refactor: replace template with component

--- src/components/layout/DashboardLayout.vue
+++ src/components/layout/DashboardLayout.vue
@@ -14,74 +14,7 @@
           'flex'
         ]"
       >
-        <div class="w-sidebar flex flex-col gap-y-1 p-4">
-          <div class="flex-1">
-            <router-link
-              :to="{ name: 'dashboard-home' }"
-              :class="[
-                ' py-2 px-2 ',
-                'flex items-center gap-1',
-                'hover:bg-gray-200 border-l-4 border-transparent rounded'
-              ]"
-              exact-active-class="border-charcoal bg-gray-200"
-            >
-              <Icon
-                :icon="
-                  $route.name === 'dashboard-home'
-                    ? 'ic:baseline-dashboard'
-                    : 'material-symbols-light:dashboard-outline'
-                "
-                :width="20"
-              />
-              <span :class="[$route.name === 'dashboard-home' ? '' : 'text-gray-600']"
-                >Dashboard</span
-              >
-            </router-link>
-            <router-link
-              :to="{ name: 'employee-list' }"
-              :class="[
-                ' py-2 px-2 ',
-                'flex items-center gap-1',
-                'hover:bg-gray-200 border-l-4 border-transparent rounded'
-              ]"
-              exact-active-class="border-charcoal bg-gray-200"
-            >
-              <Icon
-                :icon="$route.name === 'employee-list' ? 'mdi:users' : 'ph:users-light'"
-                :width="20"
-              />
-              <span :class="[$route.name === 'employee-list' ? '' : 'text-gray-600']"
-                >Employees</span
-              >
-            </router-link>
-            <router-link
-              :to="{ name: 'leave-list' }"
-              :class="[
-                ' py-0.5 px-1',
-                'flex items-center gap-1',
-                'hover:bg-gray-200 border-l-4 border-transparent rounded'
-              ]"
-              exact-active-class="border-charcoal bg-gray-200"
-            >
-              <Icon
-                :icon="
-                  $route.name === 'leave-list'
-                    ? 'bitcoin-icons:calendar-filled'
-                    : 'bitcoin-icons:calendar-outline'
-                "
-                :width="26"
-              />
-              <span :class="[$route.name === 'leave-list' ? '' : 'text-gray-600']"> Leaves</span>
-            </router-link>
-          </div>
-
-          <button :class="['flex items-center gap-x-1.5 px-2', 'text-gray-400']">
-            <span>
-              <Icon icon="solar:settings-bold" class="text-gray-400" :width="20" />
-            </span>
-            <span>Settings</span>
-          </button>
-        </div>
+        <AppSidebar />
         <div class="flex-1 h-full overflow-y-auto">
           <router-view />
         </div>
@@ -92,5 +25,5 @@
 
 <script setup lang="ts">
 import AppLogo from '@/components/shared/AppLogo.vue';
-import { Icon } from '@iconify/vue';
+import AppSidebar from '../navigation/AppSidebar.vue';
 </script>
